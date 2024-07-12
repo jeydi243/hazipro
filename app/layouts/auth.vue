c83f08f1b83e2fdff8200dea4e62a66f2958df2f
KADIONGO ILUNGA Epaphrodite <kadiongo.ilunga@gecamines.cd>
2024-07-12 11:50:43 +0200

    added supabase as composables

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-const route = useRoute()
-const appConfig = useAppConfig()
-const { isHelpSlideoverOpen } = useDashboard()
+useHead({
+  bodyAttrs: {
+    class: 'dark:bg-gray-950'
+  }
+})
 
-const defaultColors = ref(['green', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet'].map(color => ({ label: color, chip: color, click: () => appConfig.ui.primary = color })))
-const colors = computed(() => defaultColors.value.map(color => ({ ...color, active: appConfig.ui.primary === color.label })))
 </script>
 
 <template>
   <UDashboardLayout>
-    <slot />
+    <div class="h-screen flex items-center justify-center overlay w-screen">
+      <slot />
+    </div>
     <!-- ~/components/NotificationsSlideover.vue -->
     <NotificationsSlideover />
   </UDashboardLayout>
--- app/pages/auth.vue
+++ app/pages/auth.vue
@@ -1,12 +1,48 @@
 <template>
-    <UAuthForm title="Login" description="Enter your credentials to access your account." align="bottom"
-        icon="i-heroicons-user-circle"
-        :fields="[{ type: 'email', label: 'Email', placeholder: 'Enter your email', color: 'gray' }, { type: 'password', label: 'Password', placeholder: 'Enter your password', color: 'gray' }]"
-        :providers="[{ label: 'GitHub', icon: 'i-simple-icons-github', color: 'gray' }]" :loading="false" />
+    <UCard class="max-w-sm w-full bg-white/75 dark:bg-white/5 backdrop-blur">
+        <UAuthForm title="Login" description="Enter your credentials to access your account." align="bottom"
+            icon="i-heroicons-user-circle" :validate="validate"
+            :fields="[{ name: 'email', type: 'email', label: 'Email', placeholder: 'Enter your email' }, { name: 'password', type: 'password', label: 'Password', placeholder: 'Enter your password' }]"
+            :loading="false" :providers="providers" @submit="onSubmit" />
+    </UCard>
 </template>
 
 <script setup lang="ts">
+import type { AuthResponse, AuthTokenResponsePassword } from '@supabase/supabase-js';
+
+const router = useRouter()
+const supabase = useSupabase()
+const toast = useToast()
 definePageMeta({ layout: 'auth' })
+useSeoMeta({
+    title: 'Login'
+})
+const validate = (state: any) => {
+    const errors = []
+    if (!state.email) errors.push({ path: 'email', message: 'Email is required' })
+    if (!state.password) errors.push({ path: 'password', message: 'Password is required' })
+    return errors
+}
+const providers = [{
+    label: 'Continue with GitHub',
+    icon: 'i-simple-icons-github',
+    color: 'white' as const,
+    click: () => {
+        console.log('Redirect to GitHub')
+    }
+}]
+async function onSubmit(payload: any) {
+    console.log("onSubmit %o", payload);
+    const { data, error }: AuthTokenResponsePassword = await supabase.auth.signInWithPassword({ email: payload.email, password: payload.password });
+    // const response: AuthResponse = await supabase.auth.signUp({ email: data.email, password: data.password, options: {} });
+    console.log({ data });
+    if (!error) {
+        const current_user = await supabase.auth.getUser();
+        console.log({ current_user });
+        router.push('/')
+    }
+    toast.add({ icon: 'i-heroicons-check-circle', title: 'Login unseccessful', color: 'red' });
+}
 </script>
 
 <style scoped></style>
